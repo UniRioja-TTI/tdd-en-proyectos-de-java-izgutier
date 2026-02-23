34271fd3882a80cce29d1c8bb8a589b393967c4b
izgutier <izgutier@unirioja.es>
2026-02-23 16:08:25 +0100

    Add ToDo class with properties and methods

---
+++ src/main/java/com/tt1/test/ToDo.java
@@ -0,0 +1,27 @@
+package com.tt1.model;
+
+import java.time.LocalDate;
+
+public class ToDo {
+    private String nombre;
+    private String descripcion;
+    private LocalDate fechaLimite;
+
+    public ToDo() {}
+
+
+    public ToDo(String nombre, String descripcion, LocalDate fechaLimite) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fechaLimite = fechaLimite;
+    }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+
+    public LocalDate getFechaLimite() { return fechaLimite; }
+    public void setFechaLimite(LocalDate fechaLimite) { this.fechaLimite = fechaLimite; }
+}
